ceb0f304e2780d387f85acee531da1d5e70d5222
Justin Hsiao <158727277+Juni41@users.noreply.github.com>
2024-07-23 12:03:09 +1200

    added animation functions

--- GameJam-2/assets/character/scripts/character.gd
+++ GameJam-2/assets/character/scripts/character.gd
@@ -1,6 +1,12 @@
 class_name Player extends CharacterBody2D
 
 var move_speed : float = 100.0
+var cardinal_direction : Vector2 = Vector2.DOWN
+var direction : Vector2 = Vector2.ZERO
+var state : String = "idle"
+
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var sprite_2d : Sprite2D = $Sprite2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -8,12 +14,35 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var direction : Vector2 = Vector2.ZERO
+	
 	direction.x = Input.get_action_strength("Right") - Input.get_action_strength("Left")
 	direction.y = Input.get_action_strength("Down") - Input.get_action_strength("Up")
 	velocity = direction * move_speed
+	UpdateAnimation()
 	pass
 
 
 func _physics_process(delta):
 	move_and_slide()
+	
+	
+func SetDirection() -> bool:
+	
+	return true
+	
+func SetState() -> bool:
+	
+	return true
+
+func UpdateAnimation() -> void:
+	animation_player.play( state + "_" + "down")
+	pass
+	
+func AnimDirection() -> String:
+	if cardinal_direction == Vector2.DOWN:
+		return "down"
+	elif cardinal_direction == Vector2.UP:
+		return "UP"
+	else:
+		return "side"
+
